a4118f55e3da1b9ed1907fdcf7e0d3cd6e83926c
nitatemic <alexandre@nitatemic.dev>
2023-06-12 16:55:47 +0200

    Change file path

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../css/Modal.css';
+import '/src/css/Modal.css';
 
 export default function Modal({ isOpen, onClose, children }) {
     const [modalOpen, setModalOpen] = useState(isOpen);
