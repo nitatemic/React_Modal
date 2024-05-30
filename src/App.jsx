83be5787479dac694716ed45b8c4750e441943f7
nitatemic <alexandre@nitatemic.dev>
2024-05-30 11:38:51 +0200

    Removal of console.log

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import Modal from './components/Modal.jsx';
+import Modal from './components/Modal';
 import './App.css';
 
-const App = () => {
-    const [modalOpen, setModalOpen] = useState(false);
+function App() {
+  const [modalOpen, setModalOpen] = useState(false);
 
-    const openModal = () => {
-        setModalOpen(true);
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (event.target.classList.contains('modal-overlay')) {
+        closeModal();
+      }
     };
 
-    const closeModal = () => {
-        setModalOpen(false);
+    if (modalOpen) {
+      document.addEventListener('click', handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
     };
+  }, [modalOpen]);
 
-    useEffect(() => {
-        const handleOutsideClick = (event) => {
-            if (event.target.classList.contains('modal-overlay')) {
-                closeModal();
-            }
-        };
-
-        if (modalOpen) {
-            document.addEventListener('click', handleOutsideClick);
-        }
-
-        return () => {
-            document.removeEventListener('click', handleOutsideClick);
-        };
-    }, [modalOpen]);
-
-    return (
-        <div>
-            <button onClick={openModal}>Open modal</button>
-            {modalOpen && (
-                <Modal isOpen={modalOpen} onClose={closeModal} displayCross={true}>
-                    <h2 className="title-modal">Modal content</h2>
-                    <p className="content-modal">This is the content of the modal.</p>
-                </Modal>
-            )}
-        </div>
-    );
-};
+  return (
+    <div>
+      <button onClick={openModal}>Open modal</button>
+      {modalOpen && (
+        <Modal isOpen={modalOpen} onClose={closeModal} displayCross>
+          <h2 className="title-modal">Modal content</h2>
+          <p className="content-modal">This is the content of the modal.</p>
+        </Modal>
+      )}
+    </div>
+  );
+}
 
 export default App;
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,29 +1,30 @@
 import React, { useState } from 'react';
 import '../css/modal.css';
 
-export default function Modal({ isOpen, onClose, displayCross, children }) {
-    const [modalOpen, setModalOpen] = useState(isOpen);
+export default function Modal({
+  isOpen, onClose, displayCross, children,
+}) {
+  const [modalOpen, setModalOpen] = useState(isOpen);
 
-    const closeModal = () => {
-        setModalOpen(false);
-        onClose();
-    };
+  const closeModal = () => {
+    setModalOpen(false);
+    onClose();
+  };
 
-    if (!modalOpen) {
-        console.log(modalOpen);
-        console.log('Modal is closed');
-        return null;
-    }
+  if (!modalOpen) {
+    return null;
+  }
 
-    return (
-        <div className="modal-overlay">
-            <div className="modal-background">
-                {displayCross && (
-                <button className="modal-cross" onClick={closeModal}>
-                    &times;
-                </button>)}
-                {children}
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className="modal-overlay">
+      <div className="modal-background">
+        {displayCross && (
+        <button className="modal-cross" onClick={closeModal}>
+          &times;
+        </button>
+        )}
+        {children}
+      </div>
+    </div>
+  );
+}
